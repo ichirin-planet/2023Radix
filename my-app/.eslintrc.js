b47b82551d80ba8f4bbd2c0f5f76d9b2f1f0ba2e
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2023-09-03 14:53:37 +0900

    初期設定 (#1)

---
+++ my-app/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["import", "unused-imports", "@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        pathGroups: [{ pattern: "*.css", group: "type", position: "after" }],
+      },
+    ],
+  },
+};
